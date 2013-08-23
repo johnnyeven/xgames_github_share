6d148d39d15cc1a0bef8cbc619790714123d0245
johnnyeven_home <johnnyeven@gmail.com>
2013-08-24 00:20:57 +0800

    1

--- resources/jquery.github_treeview.js
+++ resources/jquery.github_treeview.js
@@ -1,34 +1,65 @@
 ( function( $ ) {
+	$
+			.extend(
+					$.fn,
+					{
+						githubTreeview : function( options ) {
 
-	$.extend( {
-		githubTreeview : function( options ) {
+							var defaultOptions = {
+								rootContainer : true,
+								container : $( this ),
+								autoWidth : true,
+								width : 0,
+								autoHeight : true,
+								height : 0,
+							};
 
-			var defaultOptions = {
-				rootContainer : true,
-				container : $( this ),
-				autoWidth : true,
-				width : 0,
-				autoHeight : true,
-				height : 0,
-			};
+							var settings = $.extend( {}, defaultOptions,
+									options );
+							if ( !settings.rootContainer ) {
+								$( this )
+										.append(
+												'<div id="github_treeview_plugin"></div>' );
+								settings.container = $( "#github_treeview_plugin" );
+							}
+							var root = settings.container;
+							root.addClass( 'xgame-gst-container' );
+							if ( settings.autoWidth ) {
+								settings.container.width( settings.container
+										.parent().width() );
+							} else {
+								settings.container.width( settings.width );
+							}
+							if ( settings.autoHeight ) {
+								settings.container.height( settings.container
+										.parent().height() );
+							} else {
+								settings.container.height( settings.height );
+							}
 
-			var settings = $.extend( {}, defaultOptions, options );
+							root
+									.append( '<div id="xgame-gst-commit-title" class="xgame-gst-commit-title"></div>' );
+							root
+									.append( '<div id="xgame-gst-commit-detail" class="xgame-gst-commit-detail"></div>' );
+							$( "#xgame-gst-commit-detail" )
+									.append(
+											'<img id="xgame-gst-commit-gravatar" class="xgame-gst-commit-gravatar" src="#" width="20" height="20" /><span id="xgame-gst-commit-author" class="xgame-gst-commit-author"></span>' );
+							root
+									.append( '<div id="xgame-gst-contents" class="xgame-gst-contents"></div>' );
+							var contentHeight = root.height()
+									- $( "#xgame-gst-commit-title" )
+											.outerHeight()
+									- $( "#xgame-gst-commit-detail" )
+											.outerHeight();
+							$( "#xgame-gst-contents" ).height(
+									contentHeight - 1 );
 
-			if ( !settings.rootContainer ) {
-				$( this ).append( '<div id="github_treeview_plugin"></div>' );
-				settings.container = $( "#github_treeview_plugin" );
-			}
-			if ( settings.autoWidth ) {
-
-			} else {
-
-			}
-			if ( settings.autoHeight ) {
-
-			} else {
-
-			}
-		}
-	} );
+							$( "#xgame-gst-contents" )
+									.append(
+											'<div id="xgame-gst-list-wrapper" class="xgame-gst-list-wrapper"></div>' );
+							$( "#xgame-gst-list-wrapper" ).append(
+									'<ul class="xgame-gst-list"></ul>' );
+						}
+					} );
 
 } )( jQuery );
